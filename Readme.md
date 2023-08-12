7be1861930e5718c5db7c2bd1479896da754343e
vicky <tyagivivek528@gmail.com>
2023-08-12 11:14:08 +0530

    created a port to listen

---
+++ Readme.md
@@ -0,0 +1,4 @@
+************NOTES ON WEBSERVER IMPLEMENTATION IN RUST*************
+
+1) First, import the TCP from std library of rust
+2) Then , make the endpoint on
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::net::TcpListener; //Used to listen any connection locally
+
 fn main() {
     println!("Basic Webserver in Rust");
+    create_tcp_listener();
+}
+
+fn create_tcp_listener() ->  {
+    //First thing webserver do is to listen any TCP connection
+
+    const HOST: &str = "127.0.0.1"; //IP address of the local machine
+    const PORT: &str = "8477"; //PORT on which TCP listens to the connection
+
+    //Now we're going to create an endpoint by concatenating, HOST & PORT
+    let endpoint: String = HOST.to_owned() + ":" + PORT; //to_owned() method basically makes a reference value to owned value or heap allocated
+
+    let listener = TcpListener::bind(endpoint);
 }
