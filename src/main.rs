96f61790c0d167249ed58531aa3a35445c91e27a
vicky <tyagivivek528@gmail.com>
2023-08-13 11:35:27 +0530

    Receiving data from the connection, handle_connection function is done

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use std::io::prelude;
+use std::io::Read;
 use std::net::TcpListener; //Used to listen any connection locally
+use std::net::TcpStream;
 
 fn main() {
     println!("Basic Webserver in Rust");
@@ -9,7 +12,7 @@ fn create_tcp_listener() {
     //First thing webserver do is to listen any TCP connection
     println!("Connecting...");
     const HOST: &str = "127.0.0.1"; //IP address of the local machine
-    const PORT: &str = "8477"; //PORT on which TCP listens to the connection
+    const PORT: &str = "7878"; //PORT on which TCP listens to the connection
 
     //Now we're going to create an endpoint by concatenating, HOST & PORT
     let endpoint: String = HOST.to_owned() + ":" + PORT; //to_owned() method basically makes a reference value to owned value or heap allocated
@@ -19,11 +22,12 @@ fn create_tcp_listener() {
     match listener {
         Ok(listener) => {
             println!("Port is listening to {}", PORT);
-            //looping over to listener to listen every connection
+            //looping over to liste ner to listen every connection, incoming() basically iterates over the incoming connections on the network
             for stream in listener.incoming() {
                 match stream {
                     Ok(_stream) => {
                         println!("Connection established with stream:{:?}", _stream);
+                        handle_connection(_stream);
                     }
                     Err(e) => {
                         println!("Error occured while connecting:{}", e);
@@ -33,6 +37,21 @@ fn create_tcp_listener() {
         }
         Err(e) => {
             println!("Err occured:{}", e);
+        }
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    let stream_res = stream.read(&mut buffer);
+
+    match stream_res {
+        Ok(res) => {
+            println!("Request:{:?}", String::from_utf8_lossy(&buffer[..res]));
+            println!("Result:{}", res);
+        }
+        Err(e) => {
+            println!("Error occured while reading the connection:{}", e);
         }
     }
 }
