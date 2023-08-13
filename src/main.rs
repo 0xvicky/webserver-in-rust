22d59535d203caae4a348c3d3d0c3e17ebe7e3f5
vicky <tyagivivek528@gmail.com>
2023-08-13 15:06:16 +0530

    Index.html is provided as response to the server, webserver is done

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use std::fs;
 use std::io::prelude;
 use std::io::Read;
+use std::io::Write;
 use std::net::TcpListener; //Used to listen any connection locally
 use std::net::TcpStream;
 
@@ -42,16 +44,46 @@ fn create_tcp_listener() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 1024];
+    let mut buffer = [0; 1024]; //array of 1024 elements of 0,
     let stream_res = stream.read(&mut buffer);
 
     match stream_res {
         Ok(res) => {
-            println!("Request:{:?}", String::from_utf8_lossy(&buffer[..res]));
+            println!("Request:{:?}", String::from_utf8_lossy(&buffer[..]));
             println!("Result:{}", res);
         }
         Err(e) => {
             println!("Error occured while reading the connection:{}", e);
+        }
+    }
+
+    //RESPONDING TO THE REQUEST FROM THE CLIENT
+    let res_content = fs::read_to_string("index.html");
+    match res_content {
+        Ok(res) => {
+            let response = format!(
+                "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+                res.len(),
+                res
+            );
+            let stream_wrt: Result<usize, std::io::Error> = stream.write(response.as_bytes()); //writing response to the connection or stream
+            match stream_wrt {
+                Ok(_) => {
+                    let stream_flsh = stream.flush(); //sending the buffer response instantly to the client if in the streamline.
+                    match stream_flsh {
+                        Ok(_) => {}
+                        Err(e) => {
+                            println!("Error occured:{}", e);
+                        }
+                    }
+                }
+                Err(e) => {
+                    println!("Error occured while writing the res:{}", e);
+                }
+            }
+        }
+        Err(e) => {
+            println!("Error occured:{}", e);
         }
     }
 }
